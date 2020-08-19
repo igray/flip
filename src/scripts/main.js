515965a70d5fd13e4f7ea738c690edbc9e2ee029
Iain Gray <igray78756@gmail.com>
2020-08-19 13:20:33 -0500

    Put initializer on window

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,9 @@
-//import Tick from '@pqina/flip'
-import '@pqina/flip/dist/flip.js'
-import '../styles/main.scss'
+import "@pqina/flip/dist/flip.js";
+import "../styles/main.scss";
 
-/*
-function setupFlip(tick) {
-  Tick.helper.interval(function() {
+window.setupFlip = function (tick) {
+  Tick.helper.interval(function () {
     tick.value++;
-    tick.root.setAttribute(
-      'aria-label', 
-      tick.value
-    );
+    tick.root.setAttribute("aria-label", tick.value);
   }, 1000);
-}
-*/
+};
